263e8104706e60bc3bc0fa4c8c8805bd8009f92f
Mrudul Kandoth <mrudul@openzone.io>
2023-06-24 10:45:58 -0400

    feat: Initial - Added VPC/Subnets with IGW and NGW

---
+++ dev/vpc/main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+  source                      = "../../modules/vpc"
+  vpc_cidr_block            = var.vpc_cidr_block
+  public_subnet_cidr_block  = var.public_subnet_cidr_block
+  private_subnet_cidr_block = var.private_subnet_cidr_block
+}
